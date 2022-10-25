beea2d4ce7872136c50cc1df2291bd61963ac11e
atomov93 <62711773+atomov93@users.noreply.github.com>
2022-10-25 15:44:29 +0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -67,6 +67,17 @@ export default function Layout({
         {...containerProps}>
         <Head>
           <title>{pageTitle}</title>
+          <!-- Hotjar Tracking Code for my site -->
+            <script>
+              (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3217553,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+                 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+             </script>
           <meta name="description" content={metaDescription ?? pageTitle} />
           <meta name="og:description" content={metaDescription ?? pageTitle} />
           <meta property="og:type" content="article" />
